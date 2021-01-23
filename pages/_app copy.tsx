4034ef674719fec493ee1caa4d83500c4a7e0fba
NOHWONDOO <nwwd0907@naver.com>
2021-01-23 17:34:30 +0900

    third commit

--- pages/_app copy.tsx
+++ pages/_app copy.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  sadfsaf
+  sadfsafzxcvzxcvzcxv
   return <Component {...pageProps} />
 }
 
